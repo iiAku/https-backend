14d7b141a396c3701560f580c1a4525ea8e25288
Yoann Gendrey <ygendrey@gmail.com>
2023-03-11 14:56:38 +0100

    feat: try

--- setup.sh
+++ setup.sh
@@ -6,12 +6,3 @@ do
   BASH_FILES="$BASH_FILES bash $F;"
 done
 eval "$BASH_GENERAL_FILES $BASH_FILES"
-
-# -- .env.example -> .env
-ENVEXF=.env.example
-if [ -f "$ENVEXF" ]; then
-  mv .env.example .env
-
-  echo "Don't forget to fill in .env file!"
-  echo "  > nano .env"
-fi
